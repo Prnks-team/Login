4fadf2c38cd51e69b77e26754af89da7af78141d
prnk1243 <151041466+prnk1243@users.noreply.github.com>
2024-01-22 10:51:04 -0500

    Update Password.js
    Signed-off-by: prnk1243 <151041466+prnk1243@users.noreply.github.com>

--- Password.js
+++ Password.js
@@ -9,7 +9,7 @@ loginButton.addEventListener("click", (e) => {
 
     if (username === "admin" && password === "password") {
         alert("You have successfully logged in.");
-        location.reload();
+        <meta http-equiv="Refresh" content="0; url='https://prnkinfoboard.matomo.cloud/index.php?module=CoreHome&action=index&idSite=1&period=day&date=yesterday#?period=day&date=yesterday&idSite=1&category=Dashboard_Dashboard&subcategory=1'" />
     } else {
         loginErrorMsg.style.opacity = 1;
     }
